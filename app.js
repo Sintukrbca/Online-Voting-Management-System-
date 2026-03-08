31d08defdc29f43bf93781d569647c098750af9d
Sintu Kumar <sintukumarraj3082@gmail.gom>
2026-03-08 20:10:48 +0530

    updated voting system code

--- app.js
+++ app.js
@@ -38,6 +38,4 @@ app.use("/admin", adminRoutes);
 
 // Server start
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server started on port http://localhost:${PORT}`);
-});
+app.listen(PORT);

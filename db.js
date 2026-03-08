f33b3f0265f48de1cc7286b2ed5c0a57291bae91
Sintu Kumar <sintukumarraj3082@gmail.gom>
2026-03-08 20:45:29 +0530

    updated voting system code

--- db.js
+++ db.js
@@ -5,13 +5,3 @@ mongoose.connect(process.env.MONGO_URI);
 mongoose.connection.on("connected", () => {
   console.log("MongoDB Connected");
 });
-
-mongoose.connection.on("error", (err) => {
-  console.log("MongoDB Error:", err);
-});
-
-mongoose.connection.on("disconnected", () => {
-  console.log("MongoDB Disconnected");
-});
-
-module.exports = mongoose.connection;
